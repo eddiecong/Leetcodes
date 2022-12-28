33f71ec2e6edd6a75954c4c7d87195aa35675342
eddiecong <eddiecong1125@gmail.com>
2022-12-28 23:29:21 +0800

    update new functions.

--- python/remove_stones.py
+++ python/remove_stones.py
@@ -1,3 +1,25 @@
+import math
+
+# Dummy solution:
 class Solution:
     def minStoneSum(self, piles: List[int], k: int) -> int:
-        pass
+        piles_num = len(piles)
+        for i in range(k):
+            piles = sorted(piles)
+            piles[-1] = math.ceil(piles[-1] / 2)
+        return sum(piles)
+
+# Python heap solution:
+import heapq
+
+class Solution:
+    def minStoneSum(self, piles: List[int], k: int) -> int:
+        heap = [-num for num in piles]
+        heapq.heapify(heap)
+        
+        for _ in range(k):
+            curr = -heapq.heappop(heap)
+            remove = curr // 2
+            heapq.heappush(heap, -(curr - remove))
+        
+        return -sum(heap)

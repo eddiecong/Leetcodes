0dde24aa0f2f3b75d7e0dc616f4b19f11ba104fc
eddiecong <eddiecong1125@gmail.com>
2023-01-04 00:40:46 +0800

    update the delete column function.

--- python/delete_columns.py
+++ python/delete_columns.py
@@ -1,3 +1,12 @@
 class Solution:
     def minDeletionSize(self, strs: List[str]) -> int:
-        pass
+        del_count = 0
+        str_length = len(strs[0])
+        for i in range(str_length):
+            last = strs[0][i]
+            for j in range(len(strs)):
+                if strs[j][i] < last:
+                    del_count += 1
+                    break
+                last = strs[j][i]
+        return del_count

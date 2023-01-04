42b1d00d8f17f0a642febb24f93950f457f15a71
eddiecong <eddiecong1125@gmail.com>
2023-01-04 23:36:49 +0800

    update the two questions of tree.

---
+++ python/tree/lowest_common_ancestor_of_a_binary_tree.py
@@ -0,0 +1,21 @@
+# Key Idea:
+# 1. Each number is unique in the tree.
+# 2. Recursively iterate the tree object.
+
+# Reference Link:
+# Question name: 236. Lowest Common Ancestor of a Binary Tree
+# https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-tree/
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        if root in (None, p, q): return root
+        left, right = (self.lowestCommonAncestor(kid, p, q)
+                       for kid in (root.left, root.right))
+        return root if left and right else left or right

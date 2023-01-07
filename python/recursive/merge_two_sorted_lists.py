76b78fc668fa973776b7ff81626f4c250991bd65
eddiecong <eddiecong1125@gmail.com>
2023-01-07 23:26:52 +0800

    update the questions of recursive.

---
+++ python/recursive/merge_two_sorted_lists.py
@@ -0,0 +1,20 @@
+# Key Idea:
+# 1. Two linked lists are not the same length.
+# 2. Solve the question recursively.
+
+# Reference Link:
+# Question name: 21. Merge Two Sorted Lists
+# https://leetcode.com/problems/merge-two-sorted-lists/
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def mergeTwoLists(self, a, b):
+        if a and b:
+            if a.val > b.val:
+                a, b = b, a
+            a.next = self.mergeTwoLists(a.next, b)
+        return a or b
